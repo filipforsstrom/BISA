0789a892fad1f39c3574d694c7dd150f53d88473
alexurtti <92301547+alexurtti@users.noreply.github.com>
2022-05-04 14:45:13 +0200

    Bisa-5-UserRolesController+Interface

--- BISA/Server/Controllers/UserRolesController.cs
+++ BISA/Server/Controllers/UserRolesController.cs
@@ -21,21 +21,76 @@ namespace BISA.Server.Controllers
         }
 
         // POST api/<UserRolesController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("{id}")]
+        public async Task<IActionResult> PromoteToStaff(int id) //bara make staff? heta bara post?
         {
+            var eventResponse = new ServiceResponseDTO<string>();
+
+            if (eventResponse.Success)
+            {
+                return Ok(eventResponse.Message);
+            }
+            else
+            {
+                return BadRequest(eventResponse.Message);
+            }
+
+        }
+
+        [HttpPost("/[action]/{id}")]
+        public async Task<IActionResult> PromoteToAdmin(int id) //bara make staff? 
+        {
+            var eventResponse = new ServiceResponseDTO<string>();
+
+            if (eventResponse.Success)
+            {
+                return Ok(eventResponse.Message);
+            }
+            else
+            {
+                return BadRequest(eventResponse.Message);
+            }
+
         }
 
         // PUT api/<UserRolesController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+
         }
 
         // DELETE api/<UserRolesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("/demoteAdmin/{id}")]
+        public async Task<IActionResult> Delete(int id)
         {
+            var eventResponse = new ServiceResponseDTO<string>();
+
+            if (eventResponse.Success)
+            {
+                return Ok(eventResponse.Message);
+            }
+            else
+            {
+                return BadRequest(eventResponse.Message);
+            }
+
+        }
+        // DELETE api/<UserRolesController>/5
+        [HttpDelete("/demoteStaff/{id}")]
+        public async Task<IActionResult> DeleteStaff(int id)
+        {
+            var eventResponse = new ServiceResponseDTO<string>();
+
+            if (eventResponse.Success)
+            {
+                return Ok(eventResponse.Message);
+            }
+            else
+            {
+                return BadRequest(eventResponse.Message);
+            }
+
         }
     }
 }
