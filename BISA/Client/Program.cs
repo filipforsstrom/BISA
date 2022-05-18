fbff6ac5ddc59117d0e17391086776336e2f5068
alexurtti <92301547+alexurtti@users.noreply.github.com>
2022-05-18 09:22:30 +0200

    Bisa-46-Created User Registration page, viewmodel and service

--- BISA/Client/Program.cs
+++ BISA/Client/Program.cs
@@ -1,5 +1,6 @@
 global using Blazored.LocalStorage;
 using BISA.Client;
+using BISA.Client.Services.AuthService;
 using BISA.Client.Services.EventService;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
@@ -11,7 +12,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped<IEventService, EventService>();
-
+builder.Services.AddScoped<IAuthService, AuthService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ BISA/Shared/ViewModels/UserRegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BISA.Shared.ViewModels
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Please input your first name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please input your first name")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Please input your last name")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Please input your a username")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please input a password")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "You need to confirm your password")]
+        [Compare(nameof(Password),ErrorMessage ="Passwords dont match")]
+        public string ValidatePassword { get; set; }
+
+
+    }
+}
