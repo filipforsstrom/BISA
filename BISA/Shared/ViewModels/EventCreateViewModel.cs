f7539b7efa5cad297f1ff4a01b0a14b21f1fc5ee
filipforsstrom <83241499+filipforsstrom@users.noreply.github.com>
2022-05-31 10:27:00 +0200

    added snackbars and validation to eventmanagement

---
+++ BISA/Shared/ViewModels/EventCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BISA.Shared.ViewModels
+{
+    public class EventCreateViewModel
+    {
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public TimeSpan Time { get; set; }
+        [Required]
+        public string? Organizer { get; set; }
+        [Required]
+        public string? Subject { get; set; }
+        [Required]
+        public string? Location { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public int EventTypeId { get; set; }
+    }
+}
