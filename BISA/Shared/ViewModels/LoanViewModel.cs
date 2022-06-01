6f70dcc8c2871b37f00c2e74f47a93ac94317238
alexurtti <92301547+alexurtti@users.noreply.github.com>
2022-06-01 13:45:38 +0200

    Bisa-108-Profile-Loans får DateTime.Min

--- BISA/Shared/ViewModels/LoanViewModel.cs
+++ BISA/Shared/ViewModels/LoanViewModel.cs
@@ -5,8 +5,8 @@ namespace BISA.Shared.ViewModels
     public class LoanViewModel
     {
         public int Id { get; set; }
-        public DateTime DateFrom { get; set; }
-        public DateTime DateTo { get; set; }
+        public DateTime Date_From { get; set; }
+        public DateTime Date_To { get; set; }
         public string? UserId { get; set; }
         public ItemEntity? Item { get; set; }
         public int ItemInventoryId { get; set; }
