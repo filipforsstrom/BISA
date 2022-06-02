2ef4e1f3480e432d14ab8d9ef82181ecca1bc171
filipforsstrom <83241499+filipforsstrom@users.noreply.github.com>
2022-06-02 14:18:39 +0200

    books exceptions

--- BISA/Server/Services/BookService/IBookService.cs
+++ BISA/Server/Services/BookService/IBookService.cs
@@ -2,8 +2,8 @@
 {
     public interface IBookService
     {
-        Task<ServiceResponseDTO<BookDTO>> GetBook(int id);
+        Task<BookDTO> GetBook(int id);
         Task<ServiceResponseDTO<BookUpdateDTO>> UpdateBook(BookUpdateDTO bookToUpdate);
-        Task<ServiceResponseDTO<BookCreateDTO>> CreateBook(BookCreateDTO bookToCreate);
+        Task<BookCreateDTO> CreateBook(BookCreateDTO bookToCreate);
     }
 }
