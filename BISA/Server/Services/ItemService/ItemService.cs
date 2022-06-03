428cf9e680984b30b0e78d1ef573e0cfc0478630
jsprolsson <jesper.p.olsson@hotmail.com>
2022-06-03 11:39:39 +0200

    exceptions in item service and try catch in item controller

--- BISA/Server/Services/ItemService/ItemService.cs
+++ BISA/Server/Services/ItemService/ItemService.cs
@@ -13,9 +13,8 @@ namespace BISA.Server.Services.ItemService
             _context = context;
         }
 
-        public async Task<ServiceResponseDTO<ItemDTO>> GetItem(int itemId)
+        public async Task<ItemDTO> GetItem(int itemId)
         {
-            ServiceResponseDTO<ItemDTO> responseDTO = new();
 
             var itemFromDb = await _context.Items
                 .Where(i => i.Id == itemId)
@@ -25,21 +24,7 @@ namespace BISA.Server.Services.ItemService
 
             if (itemFromDb == null)
             {
-                responseDTO.Success = false;
-                responseDTO.Message = "Item doesn't exist";
-                return responseDTO;
-            }
-
-            List<ItemInventoryDTO> inventory = new List<ItemInventoryDTO>();
-            foreach (var itemInventory in itemFromDb.ItemInventory)
-            {
-                inventory.Add(new ItemInventoryDTO { Id = itemInventory.Id, ItemId = itemInventory.ItemId, Available = itemInventory.Available });
-            }
-
-            List<TagDTO> tags = new List<TagDTO>();
-            foreach (var tag in itemFromDb.Tags)
-            {
-                tags.Add(new TagDTO { Id = tag.Id, Tag = tag.Tag });
+                throw new NotFoundException("Item doesn't exist.");
             }
 
             var item = new ItemDTO
@@ -47,20 +32,17 @@ namespace BISA.Server.Services.ItemService
                 Id = itemFromDb.Id,
                 Title = itemFromDb.Title,
                 Type = itemFromDb.Type,
-                Inventory = inventory,
+                Inventory = itemFromDb.ItemInventory.Select(it => new ItemInventoryDTO { Id = it.Id, Available = it.Available, ItemId = it.ItemId}).ToList(),
                 Description = itemFromDb.Description,
                 Creator = itemFromDb.Creator,
-                Tags = tags
+                Tags = itemFromDb.Tags.Select(t => new TagDTO { Id = t.Id, Tag = t.Tag }).ToList()
             };
 
-            responseDTO.Success = true;
-            responseDTO.Data = item;
-            return responseDTO;
+            return item;
         }
 
-        public async Task<ServiceResponseDTO<ItemDTO>> DeleteItem(int itemId)
+        public async Task<string> DeleteItem(int itemId)
         {
-            ServiceResponseDTO<ItemDTO> responseDTO = new();
 
             var itemToDelete = _context.Items.Where(x => x.Id == itemId)
                 .Include(i => i.Tags)
@@ -69,16 +51,13 @@ namespace BISA.Server.Services.ItemService
 
             if (itemToDelete == null)
             {
-                responseDTO.Success = false;
-                responseDTO.Message = "Item requested for deletion not found";
-                return responseDTO;
+                throw new NotFoundException("Item requested for deletion not found");
             }
 
            if(itemToDelete.ItemInventory.Any(i => i.Available == false))
             {
-                responseDTO.Success = false;
-                responseDTO.Message = "Item requested for deletion doesn't have all copies in stock.";
-                return responseDTO;
+                throw new InvalidOperationException("Item requested for deletion doesn't have all copies in stock.");
+                
             }
 
             itemToDelete.ItemInventory.Clear();
@@ -89,22 +68,16 @@ namespace BISA.Server.Services.ItemService
 
             if(createdResult > 0)
             {
-                responseDTO.Success = true;
-                responseDTO.Message = "Item deleted";
-                return responseDTO;
+                return "Item deleted";
             }
             
-            responseDTO.Success = false;
-            responseDTO.Message = "Something went wrong with requested item for deletion";
-            return responseDTO;
+            throw new InvalidOperationException("Something went wrong with requested item for deletion");
             
         }
 
 
-        public async Task<ServiceResponseDTO<List<ItemDTO>>> GetItems()
+        public async Task<List<ItemDTO>> GetItems()
         {
-            ServiceResponseDTO<List<ItemDTO>> responseDTO = new();
-
             var itemsFromDbList = await _context.Items
                 .Include(i => i.Tags)
                 .Include(i => i.ItemInventory)
@@ -112,35 +85,25 @@ namespace BISA.Server.Services.ItemService
 
             if (itemsFromDbList == null)
             {
-                responseDTO.Success = false;
-                responseDTO.Message = "List of items requested is empty.";
-                return responseDTO;
+                throw new NotFoundException("List of items requested is empty");
             }
 
-            var listOfItems = new List<ItemDTO>();
+            List<ItemDTO> listOfItems = itemsFromDbList.Select(i => new ItemDTO {
+                Id = i.Id,
+                Title = i.Title,
+                Creator = i.Creator,
+                Date = i.Date,
+                ItemInventory = i.ItemInventory.Count(),
+                Language = i.Language,
+                Publisher = i.Publisher,
+                Type = i.Type,
+                Description = i.Description,
+                Image = i.Image,
+                Tags = ConvertTagToTagDTO(i.Tags)
+            }).ToList();
 
-            foreach (var item in itemsFromDbList)
-            {
-                listOfItems.Add(
-                    new ItemDTO
-                    {
-                        Id = item.Id,
-                        Title = item.Title,
-                        Creator = item.Creator,
-                        Date = item.Date,
-                        ItemInventory = item.ItemInventory.Count(),
-                        Language = item.Language,
-                        Publisher = item.Publisher,
-                        Type = item.Type,
-                        Description = item.Description,
-                        Image = item.Image,
-                        Tags = ConvertTagToTagDTO(item.Tags)
-                    });
-            }
 
-            responseDTO.Success = true;
-            responseDTO.Data = listOfItems;
-            return responseDTO;
+            return listOfItems;
         }
 
         private List<TagDTO> ConvertTagToTagDTO(List<TagEntity> tags)
@@ -155,22 +118,17 @@ namespace BISA.Server.Services.ItemService
             return tagsAsDTOs;
         }
 
-        public async Task<ServiceResponseDTO<List<TagDTO>>> GetTags()
+        public async Task<List<TagDTO>> GetTags()
         {
-            ServiceResponseDTO<List<TagDTO>> responseDTO = new();
             var tags = await _context.Tags.ToListAsync();
 
             if(tags == null)
             {
-                responseDTO.Success = false;
-                responseDTO.Message = "No tags were found";
-                return responseDTO;
+                throw new NotFoundException("No tags were found");
             }
 
             var tagDtos = ConvertTagToTagDTO(tags);
-            responseDTO.Success = true;
-            responseDTO.Data = tagDtos;
-            return responseDTO;
+            return tagDtos;
 
         }
     }
